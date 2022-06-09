46ec27aabaa4fe92ea2673e469fcb47ccaf9cdab
MashMake <kovalenko.ilia@Inbox.ru>
2022-06-10 00:10:29 +0300

    Update mainclasses.py

--- mainclasses.py
+++ mainclasses.py
@@ -15,12 +15,16 @@ def closerList(values, parameter):
         if math.fabs(parameter - values[i]) < m:
             m = math.fabs(parameter - values[i])
             id = i
-    return values[id]
+    return id
 def interpolation(amount1, amount2, value1, value2, parameter):
+    if parameter == amount1:
+        return value1
+    if parameter == amount2:
+        return value2
     points = amount2 - amount1
     difference = value2 - value1
     point = difference / points
-    return value1 + point * (parameter - value1)
+    return round(value1 + point * (parameter - amount1), 3)
 
 class Building(object):
     def __init__(self, walls):
@@ -53,14 +57,11 @@ class Wall(object):
         return sum
     def SuppositiveConductivity(self, insideratio, outsideratio): return 1 / insideratio + self.LayersResistSum + 1 / outsideratio
 
-
-
 class Layer(object):
     def __init__(self, width, conductivity):
         self.width = width
         self.conductivity = conductivity
         self.thermalResistance = self.width / self.conductivity
-
 
 class Assembly0(object):
     def __init__(self, value, amount):
@@ -69,6 +70,8 @@ class Assembly0(object):
 
         self.value = value
         self.amount = amount
+
+        def HeatLoss(self): return self.value * self.amount
 
 class Assembly1(object):
     def __init__(self, length, amount):
@@ -87,6 +90,7 @@ class Assembly1(object):
         elif self.length <= 40: x = 0.002
         elif self.length <= 70: x = 0.0015
         else: x = 0.001
+        return x * self.amount
 
 class Assembly2(object):
     def __init__(self, value, amount):
@@ -96,23 +100,138 @@ class Assembly2(object):
         self.value = value
         self.amount = amount
 
+        def HeatLoss(self): return self.value * self.amount
+
 class Assembly3(object):
-    
-    def __init__(self, wall_type, insulation_resistance, plate_width, base_conductivity, perforated_or_NTE, parameter, value):
+    def __init__(self, insulation_resistance, plate_width, base_conductivity, assembly_type, parameter, geometrical_value):
         self.name = 'Сопряжение с перекрытиями и балконами'
         self.type = 1
-        self.conducts = [0.2, 0.6, 1.8]
-        self.resists = [1.22, 2.44, 6.1]
 
-        self.wall = wall_type
         self.resist = insulation_resistance
         self.width = plate_width
         self.cond = base_conductivity
-        self.paramtype = perforated_or_NTE
+        self.paramtype = assembly_type
         self.parameter = parameter
+        self.geom_value = geometrical_value
+
+    def HeatLoss(self, walltype):
+        match walltype:
+            case 0:
+                conducts = [0.2, 0.6, 1.8]
+                resists = [1.22, 2.44, 6.1]
+                condid = closerList(conducts, self.cond)
+                resistid = closerList(resists, self.resist)
+                match self.paramtype:
+                    case 0:
+                        temp160 = [[0.488, 0.500, 0.577], [0.477, 0.515, 0.592], [0.408, 0.444, 0.494]]
+                        temp210 = [[0.606, 0.617, 0.698], [0.594, 0.633, 0.719], [0.592, 0.522, 0.610]]
+                        return interpolation(160, 210, temp160[resistid][condid], temp210[resistid][condid], self.width) * self.geom_value
+                    case 1:
+                        match self.parameter:
+                            case 1:
+                                temp160 = [[0.300, 0.298, 0.346], [0.304, 0.315, 0.352], [0.283, 0.298, 0.323]]
+                                temp210 = [[0.379, 0.373, 0.421], [0.385, 0.396, 0.435], [0.360, 0.377, 0.406]]
+                                return interpolation(160, 210, temp160[resistid][condid], temp210[resistid][condid], self.width) * self.geom_value
+                            case 3:
+                                temp160 = [[0.188, 0.179, 0.208], [0.196, 0.196, 0.215], [0.198, 0.202, 0.215]]
+                                temp210 = [[0.238, 0.225, 0.252], [0.252, 0.250, 0.269], [0.252, 0.259, 0.273]]
+                                return interpolation(160, 210, temp160[resistid][condid], temp210[resistid][condid], self.width) * self.geom_value
+                            case 5:
+                                temp160 = [[0.142, 0.131, 0.152], [0.152, 0.148, 0.160], [0.160, 0.161, 0.169]]
+                                temp210 = [[0.179, 0.163, 0.181], [0.194, 0.188, 0.202], [0.204, 0.206, 0.215]]
+                                return interpolation(160, 210, temp160[resistid][condid], temp210[resistid][condid], self.width) * self.geom_value
+                    case 2:
+                        match self.parameter:
+                            case 1:
+                                temp160 = [[0.126, 0.081, 0.063], [0.115, 0.098, 0.094], [0.131, 0.133, 0.137]]
+                                temp210 = [[0.126, 0.072, 0.047], [0.120, 0.098, 0.093], [0.144, 0.144, 0.147]]
+                                return interpolation(160, 210, temp160[resistid][condid], temp210[resistid][condid], self.width) * self.geom_value
+                            case 3:
+                                temp160 = [[0.086, 0.038, 0.016], [0.076, 0.055, 0.049], [0.095, 0.092, 0.097]]
+                                temp210 = [[0.086, 0.028, 0.001], [0.076, 0.055, 0.050], [0.106, 0.098, 0.103]]
+                                return interpolation(160, 210, temp160[resistid][condid], temp210[resistid][condid], self.width) * self.geom_value
+            case 1:
+                conducts = [0.2, 0.6, 1.8]
+                resists = [1.5, 3.0, 6.0]
+                condid = closerList(conducts, self.cond)
+                resistid = closerList(resists, self.resist)
+                match self.paramtype:
+                    case 0:
+                        temp160 = [[0.583, 0.660, 0.838], [0.550, 0.638, 0.781], [0.472, 0.536, 0.626]]
+                        temp210 = [[0.704, 0.777, 0.958], [0.669, 0.758, 0.915], [0.580, 0.650, 0.751]]
+                        return interpolation(160, 210, temp160[resistid][condid], temp210[resistid][condid], self.width) * self.geom_value
+                    case 1:
+                        match self.parameter:
+                            case 1:
+                                temp160 = [[0.400, 0.413, 0.477], [0.346, 0.371, 0.419], [0.311, 0.338, 0.374]]
+                                temp210 = [[0.483, 0.492, 0.556], [0.429, 0.456, 0.510], [0.393, 0.421, 0.466]]
+                                return interpolation(160, 210, temp160[resistid][condid], temp210[resistid][condid], self.width) * self.geom_value
+                            case 3:
+                                temp160 = [[0.279, 0.265, 0.285], [0.225, 0.227, 0.244], [0.209, 0.219, 0.237]]
+                                temp210 = [[0.335, 0.315, 0.333], [0.281, 0.283, 0.302], [0.268, 0.279, 0.297]]
+                                return interpolation(160, 210, temp160[resistid][condid], temp210[resistid][condid], self.width) * self.geom_value
+                            case 5:
+                                temp160 = [[0.227, 0.202, 0.210], [0.173, 0.171, 0.179], [0.168, 0.171, 0.183]]
+                                temp210 = [[0.269, 0.240, 0.244], [0.219, 0.213, 0.223], [0.213, 0.219, 0.230]]
+                                return interpolation(160, 210, temp160[resistid][condid], temp210[resistid][condid], self.width) * self.geom_value
+                    case 2:
+                        temp160 = [[0.191, 0.156, 0.151], [0.158, 0.149, 0.155], [0.168, 0.173, 0.182]]
+                        temp210 = [[0.192, 0.147, 0.134], [0.166, 0.152, 0.156], [0.182, 0.184, 0.193]]
+                        return interpolation(160, 210, temp160[resistid][condid], temp210[resistid][condid], self.width) * self.geom_value
+            case 2:
+                conducts = [0.2, 0.6, 1.8]
+                resists = [1.5, 3.0, 6.0]
+                condid = closerList(conducts, self.cond)
+                resistid = closerList(resists, self.resist)
+                match self.paramtype:
+                    case 0:
+                        temp160 = [[0.583, 0.660, 0.838], [0.550, 0.638, 0.781], [0.472, 0.536, 0.626]]
+                        temp210 = [[0.704, 0.777, 0.958], [0.669, 0.758, 0.915], [0.580, 0.650, 0.751]]
+                        return interpolation(160, 210, temp160[resistid][condid], temp210[resistid][condid], self.width) * self.geom_value
+                    case 1:
+                        match self.parameter:
+                            case 1:
+                                temp160 = [[0.400, 0.413, 0.477], [0.346, 0.371, 0.419], [0.311, 0.338, 0.374]]
+                                temp210 = [[0.483, 0.492, 0.556], [0.429, 0.456, 0.510], [0.393, 0.421, 0.466]]
+                                return interpolation(160, 210, temp160[resistid][condid], temp210[resistid][condid], self.width) * self.geom_value
+                            case 3:
+                                temp160 = [[0.279, 0.265, 0.285], [0.225, 0.227, 0.244], [0.209, 0.219, 0.237]]
+                                temp210 = [[0.335, 0.315, 0.333], [0.281, 0.283, 0.302], [0.268, 0.279, 0.297]]
+                                return interpolation(160, 210, temp160[resistid][condid], temp210[resistid][condid], self.width) * self.geom_value
+                            case 5:
+                                temp160 = [[0.227, 0.202, 0.210], [0.173, 0.171, 0.179], [0.168, 0.171, 0.183]]
+                                temp210 = [[0.269, 0.240, 0.244], [0.219, 0.213, 0.223], [0.213, 0.219, 0.230]]
+                                return interpolation(160, 210, temp160[resistid][condid], temp210[resistid][condid], self.width) * self.geom_value
+                    case 2:
+                        temp160 = [[0.191, 0.156, 0.151], [0.158, 0.149, 0.155], [0.168, 0.173, 0.182]]
+                        temp210 = [[0.192, 0.147, 0.134], [0.166, 0.152, 0.156], [0.182, 0.184, 0.193]]
+                        return interpolation(160, 210, temp160[resistid][condid], temp210[resistid][condid], self.width) * self.geom_value
+
+class Assembly4(object):
+    def __init__(self, value, amount):
+        self.name = 'Стыки панелей'
+        self.type = 1
+
+        self.value = value
+        self.amount = amount
+
+        def HeatLoss(self): return self.value * self.amount
+
+class Assembly5(object):
+    def __init__(self, insulation_resistance, plate_width, base_conductivity, assembly_type, geometrical_value, value):
+        self.name = 'Стыки с оконными блоками'
+        self.type = 1
+
+        self.resist = insulation_resistance
+        self.width = plate_width
+        self.cond = base_conductivity
+        self.paramtype = assembly_type
+        self.geom_value = geometrical_value
         self.value = value
 
-    # def HeatLoss(self):
-    #     if self.paramtype == 0:
-    #         if self.cond in self.conducts:
+    def HeatLoss(self, walltype):
+        match walltype:
+            case 0:
+                
+
 
