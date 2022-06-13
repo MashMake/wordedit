d64ca55d37515e9af9be62bf7e1d6e0fab63fbd7
MashMake <kovalenko.ilia@Inbox.ru>
2022-06-13 18:12:12 +0300

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -2,10 +2,13 @@ import mainclasses
 from tkinter import *
 from os import *
 from tkinter import filedialog
+from tkinter import ttk
 
 window = Tk()
 window.geometry('1620x1080')
+window.title('Biba')
 
+tab_control = ttk.Notebook(window)
 
 
 window.mainloop()
