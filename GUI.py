7763b3f70752d5ebb4d64c57ff5d62c42bfa500d
MashMake <kovalenko.ilia@Inbox.ru>
2022-06-16 16:36:24 +0300

    refactoring

--- GUI.py
+++ GUI.py
@@ -1,3 +1,4 @@
+from msilib.schema import CheckBox
 import mainclasses as mc
 from tkinter import *
 from os import *
@@ -5,6 +6,9 @@ from tkinter import filedialog
 from tkinter import ttk
 from tkinter.ttk import Checkbutton
 from tkinter.ttk import Combobox
+from docx import Document
+from docx.shared import Mm
+from docx.enum.text import WD_ALIGN_PARAGRAPH
 
 
 wa = 3
@@ -43,33 +47,36 @@ for i in range(wa):
     ten[i]['values'] = ('Трехслойные стены', 'СФТК', 'Системы наружной теплоизоляции')
     ten[i].grid(column=1, row=2)
 
-# le = []
-# ll = []
-# for i in range(wa):
-#     ll.append(Label(walltabs[i], text='Слои (толщина теплопроводность)'))
-#     ll[i].grid(column=0, row=3)
-#     le.append(Entry(walltabs[i], width=10))
-#     le[i].grid(column=1, row=3)
-
 a0 = []
 a1 = []
 a2 = []
 a3 = []
 a4 = []
 a5 = []
-a6 = []
+a0c = []
+a1c = []
+a2c = []
+a3c = []
+a4c = []
+a5c = []
 for i in range(wa):
-    a0.append(Checkbutton(walltabs[i], text='Гибкие связи'))
+    a0c.append(IntVar())
+    a0.append(Checkbutton(walltabs[i], text='Гибкие связи', variable=a0c[i]))
     a0[i].grid(column=0, row=4)
-    a1.append(Checkbutton(walltabs[i], text='Тарельчатый анкер'))
+    a1c.append(IntVar())
+    a1.append(Checkbutton(walltabs[i], text='Тарельчатый анкер', variable=a1c[i]))
     a1[i].grid(column=0, row=6)
-    a2.append(Checkbutton(walltabs[i], text='Кронштейн'))
+    a2c.append(IntVar())
+    a2.append(Checkbutton(walltabs[i], text='Кронштейн', variable=a2c[i]))
     a2[i].grid(column=0, row=8)
-    a3.append(Checkbutton(walltabs[i], text='Сопряжение с перекрытиями и балконами'))
+    a3c.append(IntVar())
+    a3.append(Checkbutton(walltabs[i], text='Сопряжение с перекрытиями и балконами', variable=a3c[i]))
     a3[i].grid(column=0, row=10)
-    a4.append(Checkbutton(walltabs[i], text='Стыки с оконными блоками'))
+    a4c.append(IntVar())
+    a4.append(Checkbutton(walltabs[i], text='Стыки с оконными блоками', variable=a4c[i]))
     a4[i].grid(column=0, row=14)
-    a5.append(Checkbutton(walltabs[i], text='Примыкание к цокольному ограждению'))
+    a5c.append(IntVar())
+    a5.append(Checkbutton(walltabs[i], text='Примыкание к цокольному ограждению', variable=a5c[i]))
     a5[i].grid(column=0, row=18)
 
 
@@ -235,12 +242,118 @@ for i in range(wa):
 #
 
 
+lnl = Label(maintab, text='Слои:')
+lnl.grid(column=0, row=1)
+
+l1c = IntVar()
+l1l = Checkbutton(maintab, text='Слой 1', variable=l1c)
+l1l.grid(column=0, row=2)
+l2c = IntVar()
+l2l = Checkbutton(maintab, text='Слой 2', variable=l2c)
+l2l.grid(column=0, row=3)
+l3c = IntVar()
+l3l = Checkbutton(maintab, text='Слой 3', variable=l3c)
+l3l.grid(column=0, row=4)
+l4c = IntVar()
+l4l = Checkbutton(maintab, text='Слой 4', variable=l4c)
+l4l.grid(column=0, row=5)
+
+l1nl = Label(maintab, text='Название')
+l1nl.grid(column=1, row=2)
+l2nl = Label(maintab, text='Название')
+l2nl.grid(column=1, row=3)
+l3nl = Label(maintab, text='Название')
+l3nl.grid(column=1, row=4)
+l4nl = Label(maintab, text='Название')
+l4nl.grid(column=1, row=5)
+
+l1nen = Entry(maintab, width=30)
+l1nen.grid(column=2, row=2)
+l2nen = Entry(maintab, width=30)
+l2nen.grid(column=2, row=3)
+l3nen = Entry(maintab, width=30)
+l3nen.grid(column=2, row=4)
+l4nen = Entry(maintab, width=30)
+l4nen.grid(column=2, row=5)
+
+l1dl = Label(maintab, text='Диаметр')
+l1dl.grid(column=3, row=2)
+l2dl = Label(maintab, text='Диаметр')
+l2dl.grid(column=3, row=3)
+l3dl = Label(maintab, text='Диаметр')
+l3dl.grid(column=3, row=4)
+l4dl = Label(maintab, text='Диаметр')
+l4dl.grid(column=3, row=5)
+
+l1den = Entry(maintab, width=10)
+l1den.grid(column=4, row=2)
+l2den = Entry(maintab, width=10)
+l2den.grid(column=4, row=3)
+l3den = Entry(maintab, width=10)
+l3den.grid(column=4, row=4)
+l4den = Entry(maintab, width=10)
+l4den.grid(column=4, row=5)
+
+l1cl = Label(maintab, text='Теплопроводность')
+l1cl.grid(column=5, row=2)
+l2cl = Label(maintab, text='Теплопроводность')
+l2cl.grid(column=5, row=3)
+l3cl = Label(maintab, text='Теплопроводность')
+l3cl.grid(column=5, row=4)
+l4cl = Label(maintab, text='Теплопроводность')
+l4cl.grid(column=5, row=5)
+
+l1cen = Entry(maintab, width=10)
+l1cen.grid(column=6, row=2)
+l2cen = Entry(maintab, width=10)
+l2cen.grid(column=6, row=3)
+l3cen = Entry(maintab, width=10)
+l3cen.grid(column=6, row=4)
+l4cen = Entry(maintab, width=10)
+l4cen.grid(column=6, row=5)
+
+
+popel = Label(maintab, text='Покрытия и перекрытия:')
+popel.grid(column=0, row=6)
+
+cl = Label(maintab, text='Коэф.однородности')
+cl.grid(column=0, row=7)
+
+cen = Entry(maintab, width=10)
+cen.grid(column=1, row=7)
+
+
+wdl = Label(maintab, text='Окна и двери:')
+wdl.grid(column=0, row=8)
+
+wdsl = Label(maintab, text='Площадь')
+wdsl.grid(column=0, row=9)
+
+wdsen = Entry(maintab, width=10)
+wdsen.grid(column=1, row=9)
+
+wdrl = Label(maintab, text='Приведённое сопротивление')
+wdrl.grid(column=0, row=10)
+
+wdren = Entry(maintab, width=10)
+wdren.grid(column=1, row=10)
+
+
+
 def generate():
-    window.quit()
+    print('processing')
+    doc = Document('0.docx')
+    
+
+
+
+
+
+
+
 
 AcceptButton = Button(maintab, text='Расчитать', command=generate)
 AcceptButton.grid(column=0, row=0)
-
 
 
 
